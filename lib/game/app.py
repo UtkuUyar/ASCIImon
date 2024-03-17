8144bc0cc11db7f2d3cb31bd36c1b3df772a0ce7
UtkuUyar <uyaru19@itu.edu.tr>
2024-03-17 17:36:04 +0300

    Initial project structure

---
+++ lib/game/app.py
@@ -0,0 +1,6 @@
+class ASCIImon:
+    def __init__():
+        return
+    
+    def run():
+        return
---
+++ main.py
@@ -0,0 +1,4 @@
+from lib.game.app import ASCIImon
+
+if __name__ == "__main__":
+    ASCIImon.run()
